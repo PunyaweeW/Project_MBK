e31bf71a3088fe465db476cf1fbd3f7842449cba
Phrawst <chslashout@gmail.com>
2020-01-05 08:10:27 +0700

    TEST API

--- mbk-apiV2/server.js
+++ mbk-apiV2/server.js
@@ -1,23 +1,47 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
+const moment = require('moment');
+app = express();
+bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+port = process.env.PORT || 3000;
+//enable cors
+app.use(cors())
 
-  app = express(),
-  bodyParser = require('body-parser');
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
-  port = process.env.PORT || 3000;
+console.log('prepare connection')
+// connection configurations
+const mc = mysql.createConnection({
+
+    host: '137.116.130.1',
+    user: 'root',
+    password: 'Mi7Da15s4',
+    database: 'sparepart'
+});
 
  
-console.log('use cors')
-//enable cors 
-app.use(cors())
 app.get("/url", (req, res, next) => {
  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
 });
-console.log('API server started on: ' + port);
- 
 
-// Start server
-app.listen(process.env.PORT || 3000, "0.0.0.0")
-// 
+app.get("/test", (req, res) => {
+  try{
+mc.query('SELECT * FROM part_group', (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.send(results)
+  })
+
+  }catch(err){
+    console.log("an error occurs")
+  }
+ });
+
+//server running+++++++++++++++++++++++++++++++++++
+console.log('API server started on: ' + port);
+app.listen(process.env.PORT || 3000, "0.0.0.0") 
+
+
+ 
