4511a2a5fd216d116adcf2f5b365e3b9d8d836bf
Phrawst <42211154+Phrawst@users.noreply.github.com>
2020-01-08 20:01:19 +0700

    Update server.js

--- mbk-apiV2/server.js
+++ mbk-apiV2/server.js
@@ -162,11 +162,7 @@ app.get("/loggings",function(req,res){
 
 //GET log by time
 app.get("/logging",function(req,res){
-<<<<<<< HEAD
- pool.query('SELECT barcode , COUNT(action) AS sales FROM log WHERE action = 20 AND datetime BETWEEN ? AND ? GROUP BY barcode , action',[req.body.startDate,req.body.endDate], (error, results) => {
-=======
  pool.query('SELECT barcode , COUNT(action) AS sales FROM log WHERE action=20 AND datetime BETWEEN ? AND ? GROUP BY barcode , action',[req.body.startDate,req.body.endDate], (error, results) => {
->>>>>>> 8e434df28e9148eaf9dcf374c0a23cad58af16ea
     if (error) {
       //response.send("cannot create new log, please check specified barcode")
       res.send(error)
