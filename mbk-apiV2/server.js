5aefbcee7cc65439450bae2e93ad986c21795724
Phrawst <chslashout@gmail.com>
2020-01-13 21:20:52 +0700

    API V2.10.1

--- mbk-apiV2/server.js
+++ mbk-apiV2/server.js
@@ -51,10 +51,42 @@ var Part = function(part){
 var Log = function(log){
   this.groupId = log.groupId;
   this.barcode = log.barcode;
-  this.datetime =  new Date();
-  this.action = log.action;
+  this.datetime =  NOW();
+  this.actionId = log.actionId;
 };
+//handling datetime+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function NOW() {
 
+    var date = new Date();
+    var aaaa = date.getFullYear();
+    var gg = date.getDate();
+    var mm = (date.getMonth() + 1);
+
+    if (gg < 10)
+        gg = "0" + gg;
+
+    if (mm < 10)
+        mm = "0" + mm;
+
+    var cur_day = aaaa + "-" + mm + "-" + gg;
+
+    var hours = date.getHours()
+    var minutes = date.getMinutes()
+    var seconds = date.getSeconds();
+
+    if (hours < 10)
+        hours = "0" + hours;
+
+    if (minutes < 10)
+        minutes = "0" + minutes;
+
+    if (seconds < 10)
+        seconds = "0" + seconds;
+
+    return cur_day + " " + hours + ":" + minutes + ":" + seconds;
+
+}
+//end of handling datetime------------------------------------------------
 
 //end of model section----------------------------------------------------
 //handling section++++++++++++++++++++++++++++++++++++++++++++++++++++++++
