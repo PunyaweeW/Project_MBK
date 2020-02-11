12debc4e7226c037b085ee449eb517345f5d0031
Phrawst <chslashout@gmail.com>
2020-02-11 20:40:33 +0700

    ProjectV3

--- mbkV2/src/app/app-routing.module.ts
+++ mbkV2/src/app/app-routing.module.ts
@@ -1,13 +1,31 @@
-import { ModuleWithProviders }  from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { Routes, RouterModule, ExtraOptions, PreloadAllModules } from '@angular/router';
 //components in app
-import {DashboardComponent} from "../app/dashboard/dashboard.component"
-import {NotificationComponent} from "../app/notification/notification.component";
-import {AddItemComponent} from "../app/add-item/add-item.component"
+import { DashboardComponent } from "../app/dashboard/dashboard.component"
+import { NotificationComponent } from "../app/notification/notification.component";
+import { AddItemComponent } from "../app/add-item/add-item.component"
+import { StockpageComponent } from './stockpage/stockpage.component';
+import { LogComponent } from './log/log.component';
+import { OrderingComponent } from './ordering/ordering.component';
 const routes: Routes = [
-  {path: '', component: DashboardComponent},
+  { path: '', component: DashboardComponent },
   { path: 'notification', component: NotificationComponent },
-  {path: 'addItem' , component:AddItemComponent }
+  { path: 'addItem', component: AddItemComponent },
+  { path: 'stock', component: StockpageComponent },
+  { path: 'log', component: LogComponent },
+
+  { path: 'ordering', component: OrderingComponent }
 
 ];
-export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
+@NgModule({
+  imports: [RouterModule.forRoot(routes,
+    {
+      preloadingStrategy: PreloadAllModules // <-This is our preloading
+    }
+
+  )
+],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+// export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
--- mbkV2/src/app/sidenav/sidenav.component.ts
+++ mbkV2/src/app/sidenav/sidenav.component.ts
@@ -1,29 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { DataserviceService} from "../../app/dataservice.service";
+import { DataserviceService } from "../../app/dataservice.service";
+import { getElementIndex } from '@syncfusion/ej2-angular-grids';
+import { MbkApiService } from '../mbk-api.service';
+
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent implements OnInit {
-expand = false;
-message = "test";
-subscription : Subscription;
-  menuitems = [
-  {path: '', component:"Dashboard",icon:"home"},
-  {path: 'notification' , component:"Notifications",icon:"home"},
-  {path: 'addItem' , component:"New item",icon:"home"}
-];
-  constructor(private dataService : DataserviceService) {
-  
-   }
+export class SidenavComponent implements OnInit, AfterViewInit {
 
-  ngOnInit() {
-    this.subscription = this.dataService.getMessage().subscribe(message => {
-      console.log("fetch")
-      this.message = message;
-     });
+  expand = false;
+  notifyNO = 0;
+  orderingNO = 0;
+  message = "test";
+  notiSubscription: Subscription;
+  orderingSubscription: Subscription;
+
+  menuitems = [
+    { path: '', component: "แดชบอร์ด", icon: "dashboard", text: "dashboard", aId: "ancDashboard" },
+    { path: 'notification', component: "แจ้งเตือน", icon: "notifications", text: "notification", aId: "ancNotification" },
+    { path: 'ordering', component: "สั่งซื้ออะไหล่", icon: "add_shopping_cart", text: "ordering", aId: "ancOrdering" },
+    { path: 'addItem', component: "เพิ่มอะไหล่", icon: "add_box", text: "addItem", aId: "ancAddItem" },
+    { path: 'stock', component: "คลังอะไหล่", icon: "store", text: "stock", aId: "ancStock" },
+    { path: 'log', component: "ล็อก", icon: "history", text: "log", aId: "anclog" }
+
+  ];
+  constructor(private dataService: DataserviceService, private mbkApiService: MbkApiService) {
+
   }
 
+  ngOnInit() {
+    //this.mbkApiService.reconnect().subscribe((part: any) => { });
+    var autoSaveInterval = setInterval(() => { console.log('refresh api'); this.mbkApiService.reconnect().subscribe((part: any) => { }) }, 300000);
+    this.notiSubscription = this.dataService.getNoti().subscribe(notiNo => {
+      console.log("fetch notifications")
+      this.notifyNO = notiNo;
+    });
+    this.orderingSubscription = this.dataService.getOrdering().subscribe(numberOrdering => {
+      console.log("fetch ordering")
+      this.orderingNO = numberOrdering;
+    });
+    console.log("app started")
+
+
+  }
+  ngAfterViewInit(): void {
+    //tmepolary
+    //load notification and ordering component first
+    // console.log("Method not implemented.");
+    //var anchorNoti = <HTMLAnchorElement>document.getElementById("ancNotification");
+    // var anchorOrder =<HTMLAnchorElement>document.getElementById("ancOrdering");
+    // var anchorDashboard =<HTMLAnchorElement>document.getElementById("ancDashboard");
+    // console.log(anchorNoti.id)
+    // anchorNoti.click();
+    // anchorOrder.click();
+    // anchorDashboard.click();
+  }
 }
